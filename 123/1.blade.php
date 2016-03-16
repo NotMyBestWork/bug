203d341e2dc77ee4d23858e953032486f29348d8
jetbrains <dmitry.tronin@jetbrains.com>
2016-03-16 10:34:45 +0300

    123

---
+++ 123/1.blade.php
@@ -0,0 +1,10 @@
+<php
+        use User;
+
+        class SomeUserClass
+        {
+        public function __construct(User $user) //here is the use of the class
+{
+//some code here
+}
+}
---
+++ 123/indx.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jetbrains
+ * Date: 1/21/2016
+ * Time: 12:28 PM
+ */
+
+use User;
+
+class SomeUserClass
+{
+	public function __construct(User $user) //here is the use of the class
+	{
+//some code here
+	}
+}
